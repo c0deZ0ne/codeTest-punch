b5fdbd7e8bf13db8915b1ca3460a69cd75f42b47
dogubo mamuzo joshua <51678953+c0deZ0ne@users.noreply.github.com>
2024-04-11 10:54:18 +0100

    adjust bitton

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,13 +20,17 @@ const Hero = () => {
 
 
       <div className='relative h-60 mt-12 rounded-2xl flex justify-center items-center'>
-  <div className='bg-[#f8f8f8] w-full lg:mr-40 h-60 lg:ml-40 rounded-xl'>
-    <ul className='absolute inset-x-4 top-4 flex justify-center items-center gap-x-[10px]'>
-      <li className='bg-[#c9efc4] p-2 rounded-xl font-bold pl-5 pr-5 capitalize'>IT & development</li>
-      <li className='p-2 bg-[#f2f2f2] rounded-xl text-[#898a8d] pl-5 pr-5 capitalize'>design and creative</li>
-    </ul>
+        <div className='bg-[#f8f8f8] relative w-full lg:mr-40 h-60 lg:ml-40 rounded-xl flex flex-col justify-evenly gap-y-[20px]'>
+          
+          <ul className=' relative inset-x-4  flex justify-center    items-center align-middle mr-auto ml-auto  w-[100%] '>
+            <span className='flex justify-center align-middle self-center mr-auto ml-auto bg-[#D2D2D2] rounded-xl '>
+               <li className=' hover:bg-[#c9efc4] rounded-xl p-2  font-bold pl-5 pr-5 capitalize  text-[#898a8d] hover:text-[#202229] '>IT & development</li>
+           
+               <li className=' hover:bg-[#c9efc4] rounded-xl p-2  font-bold pl-5 pr-5 capitalize  text-[#898a8d] hover:text-[#202229] '>Design and creativet</li>
+            </span>
+          </ul>
 
-    <div className="flex justify-between px-10  mt-20 lg:px-0 lg:mr-20 lg:ml-20">
+    <div className="flex justify-between px-10  lg:px-0 lg:mr-20 lg:ml-20">
       <div className="text-gray-500 text-md capitalize flex text-sm flex-col ">
         <ul className='justify-start list-none '>
           <li className='mt-2'>python developer</li>
