d2501c52962f342bec743f7954b2a14948b26af5
dogubo mamuzo joshua <51678953+c0deZ0ne@users.noreply.github.com>
2024-04-11 11:58:01 +0100

    reduce duration time

--- src/ui-components/ActionButton.jsx
+++ src/ui-components/ActionButton.jsx
@@ -3,11 +3,11 @@ import { smallLogo } from '../assets/icons';
 
 function ActionButton({link,text}) {
   return (
-		   <a href={link||"#"} className='flex align-middle items-center h-[50px] rounded-2xl  hover:bg-[#202229] w-[85%] transition duration-400 ease-linear group'>
+		   <a href={link||"#"} className='flex align-middle items-center h-[50px] rounded-2xl  hover:bg-[#202229] w-[85%] transition duration-300 ease-linear group'>
           <div className='flex bg-[#202229] w-[30%] items-center align-middle h-[50px] rounded-2xl flex-row justify-center'>
-            <span  className='flex transition duration-400 ease-linear   group group-hover:translate-x-[120px] '>{ smallLogo()}</span>
+            <span  className='flex transition duration-300 ease-linear   group group-hover:translate-x-[120px] '>{ smallLogo()}</span>
           </div>
-			  <span className='flex  h-[100%] items-center align-middle w-[60%] transition duration-400 ease-linear group group-hover:text-white  group-hover:translate-x-[-50%]  px-[15px]'> <pre className='font-normal font-sans flex ' > {text}</pre></span>
+			  <span className='flex  h-[100%] items-center align-middle w-[60%] transition duration-300 ease-linear group group-hover:text-white  group-hover:translate-x-[-50%]  px-[15px]'> <pre className='font-normal font-sans flex ' > {text}</pre></span>
         </a>
   )
 }
